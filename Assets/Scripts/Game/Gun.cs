b248b1e7fcfbd4da89ee5c170207cf5e1138e8f4
limlida <18578650506@163.com>
2018-05-24 17:16:15 +0800

    player

--- Assets/Scripts/Game/Gun.cs
+++ Assets/Scripts/Game/Gun.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour {
-
+    private const string Path = "Characters/Chevalier/Prefabs/Gun_fire";
     public float Force = 1000f;
-    private float speed = 1;
     private Vector2 currentPosition;
     private SpriteRenderer spriteR;
     private float angle;
+    private GameObject gunType;
+
 
     // Use this for initialization
     void Start() {
         currentPosition = transform.parent.position;
-        spriteR = GetComponent<SpriteRenderer>();
+        spriteR = this.gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,9 +26,10 @@ public class Gun : MonoBehaviour {
             Debug.Log("error");
         }
 
+        //TODO gunTpye
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            spriteR.sprite = Resources.Load<Sprite>("Gun1");
+            spriteR.sprite = Resources.Load<Sprite>("Models/Gun1");//TODO gunTpye
             transform.position = new Vector2(transform.parent.position.x + 0.25f, transform.position.y);
 
             angle = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
@@ -36,7 +38,7 @@ public class Gun : MonoBehaviour {
         }
         else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            spriteR.sprite = Resources.Load<Sprite>("Gun0");
+            spriteR.sprite = Resources.Load<Sprite>("Models/Gun0");
             transform.position = new Vector2(transform.parent.position.x - 0.25f, transform.position.y);
 
             angle = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
@@ -55,7 +57,7 @@ public class Gun : MonoBehaviour {
             //nextFire = Time.time + fireRate;
             fire();
             GameObject bullet = BulletsPool.bulletsPoolInstance.GetPooledObject();//获取对象池中的子弹
-            GameObject gunPos = GameObject.Find("Gun/Position");
+            GameObject gunPos = GameObject.Find("Gun_Position");
 
             if (bullet != null)//不为空时执行
             {
@@ -63,12 +65,12 @@ public class Gun : MonoBehaviour {
                 if (GameObject.Find("Gun").GetComponent<SpriteRenderer>().sprite.name.Equals("Gun0"))
                 {
                     bullet.transform.position = gunPos.transform.position;//初始化子弹的位置
-                    bullet.GetComponent<Rigidbody2D>().AddForce(-Vector2.right * Force);//TODO
+                    bullet.GetComponent<Rigidbody2D>().AddForce(-transform.right * Force);//TODO
                 }
                 else if (GameObject.Find("Gun").GetComponent<SpriteRenderer>().sprite.name.Equals("Gun1"))
                 {
                     bullet.transform.position = gunPos.transform.position;//初始化子弹的位置
-                    bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.right * Force);//TODO
+                    bullet.GetComponent<Rigidbody2D>().AddForce(transform.right * Force);//TODO
                 }
             }
         }
@@ -81,25 +83,25 @@ public class Gun : MonoBehaviour {
        
         if (spriteR.sprite.name.Equals("Gun1"))
         {
-            GameObject gun_fire = (GameObject)Resources.Load("Prefabs/Gun_fire");
+            GameObject gun_fire = (GameObject)Resources.Load(path: Path);
             SpriteRenderer fire = gun_fire.GetComponent<SpriteRenderer>();
             fire.sprite = Resources.Load<Sprite>("Effects/Boom/boom");
             gun_fire = Instantiate(gun_fire);
             gun_fire.transform.parent = this.transform;
             gun_fire.transform.position = new Vector2(transform.position.x, transform.position.y);
-            gun_fire.transform.localPosition = new Vector2(0.85f,0.2f);
+            gun_fire.transform.localPosition = new Vector2(0.65f,0.2f);
             gun_fire.transform.localRotation = Quaternion.identity;
         }
 
         else if(spriteR.sprite.name.Equals("Gun0"))
         {
-            GameObject gun_fire = (GameObject)Resources.Load("Prefabs/Gun_fire");
+            GameObject gun_fire = (GameObject)Resources.Load(path: Path);
             SpriteRenderer fire = gun_fire.GetComponent<SpriteRenderer>();
             fire.sprite = Resources.Load<Sprite>("Effects/Boom/boom");
             gun_fire = Instantiate(gun_fire);
             gun_fire.transform.parent = this.transform;
             gun_fire.transform.position = new Vector2(transform.position.x, transform.position.y);
-            gun_fire.transform.localPosition = new Vector2(-0.85f, 0.15f);
+            gun_fire.transform.localPosition = new Vector2(-0.65f, 0.15f);
             gun_fire.transform.localRotation = Quaternion.identity;
         }
     }
--- Assets/Scripts/Game/GunPosition.cs
+++ Assets/Scripts/Game/GunPosition.cs
@@ -9,7 +9,7 @@
  
  	// Use this for initialization
  	void Start () {
-         gunObj = GameObject.FindGameObjectWithTag("Gun");
+         gunObj = GameObject.Find("Gun");
          gunPos = gunObj.transform.position;
          this.transform.parent = gunObj.transform;
  	}
@@ -19,19 +19,20 @@
          if (!gunObj)
          {
              Debug.Log("Gun is null");
+             return;
          }
  
          if(gunObj.GetComponent<SpriteRenderer>().sprite.name == "Gun1")
          {
              this.transform.position = gunPos;
-                
-             this.transform.localPosition = new Vector2(0.65f, 0f);
+             this.transform.localRotation = gunObj.transform.rotation;
+             this.transform.localPosition = new Vector2(0.65f, 0.2f);
          }
          else if(gunObj.GetComponent<SpriteRenderer>().sprite.name == "Gun0")
          {
              this.transform.position = gunPos;
-             
-             this.transform.localPosition = new Vector2(-0.65f, 0f);
+             this.transform.localRotation = gunObj.transform.rotation;
+             this.transform.localPosition = new Vector2(-0.65f, 0.2f);
          }
  
          
